4200b3ec42b328ab0baf6405af480a4ac07617ad
RomainC <romain.caplier@student-cs.fr>
2021-10-27 13:42:28 +0200

    Added subscribe and unsubscribe commands

--- src/functions.py
+++ src/functions.py
@@ -1,41 +1,31 @@
 import numpy as np
+import pandas as pd
 import pickle
 import os
+import uuid
 from os import path
 
 # ------------------------------------------------------------------------------
 # Utilities :
 
-def chain(list_names):
+
+def grab_updates():
     """
-    Input: List of names ('@username') of participants
-    Output: Dict {'giver': 'receiver'} with giver and receiver being usernames
+    Periodicly called to fetch updates from the card
+    Updates the status command (machines used, time left estimated, users currently using)
     """
-
-    np.random.shuffle(list_names)
-
-    list_couples = []
-
-    for i in range(len(list_names) - 1):
-        list_couples.append((list_names[i], list_names[i+1]))
-
-    list_couples.append((list_names[-1], list_names[0]))
-
-    dict_couples = {}
-    for couple in list_couples:
-        dict_couples[couple[0]] = couple[1]
-
-    return dict_couples
+    pass
 
 # ------------------------------------------------------------------------------
 # Handlers
+
 
 def start(update, context):
     """
     Welcome message on /start
     """
     context.bot.send_message(chat_id=update.effective_chat.id,
-                             text="Welcome to bot-Santa!\nTo join the pool of participants, use /join.\nTo see the list of participants, use /list. \nTo get the name of the person that was drawn for you, use /get_name. \n")
+                             text="Welcome to the Smart Washing Mashine Bot!\nAvaliable commands:\n- /subscribe: Provides you with a NFC tag to use the machines\n- /unsubscribe: Removes your tag and username from the database\n- /status: Displays the status of all connected machines")
 
 
 def reset(update, context):
@@ -44,148 +34,76 @@ def reset(update, context):
     Cleans up the data folder
     """
     try:
-        if path.exists("data/participants.pkl"):
-            os.remove('data/participants.pkl')
-        if path.exists("data/assignements.pkl"):
-            os.remove('data/assignements.pkl')
+        if path.exists("data/subscribers.pkl"):
+            os.remove('data/subscribers.pkl')
+        if path.exists("data/status.pkl"):
+            os.remove('data/status.pkl')
         context.bot.send_message(chat_id=update.message.chat_id,
                                  text="Reset done")
     except:
         context.bot.send_message(chat_id=update.message.chat_id,
-                                 text="Error, please check https://github.com/RomainC123/bot-santa and add a pull request")
+                                 text="Error, please check https://github.com/RomainC123/smart-washing-machine and add a pull request (don't do that)")
 
 
-def add(update, context):
+def connect(update, context):
+    pass
+
+
+def subscribe(update, context):
     """
-    PROTECTED
-    Adds a list of users (provided in the command, /add @username1 @username2) to the list of participants
+    Adds the username of the sender of the request to the list of subscribers, and creates an id for that person
     """
-    if not path.exists("data/assignements.pkl"):
-        try:
-            with open('data/participants.pkl', 'rb') as f:
-                list_participants = pickle.load(f)
-        except:
-            list_participants = []
+    try:
+        with open('data/subscribers.pkl', 'rb') as f:
+            df_subscribers = pickle.load(f)
+    except:
+        df_subscribers = pd.DataFrame(columns=['username', 'nfc_id'])
 
-        list_names_to_add = context.args
-        for name in list_names_to_add:
-            if name not in list_participants:
-                list_participants.append(name)
+    print(df_subscribers['username'].values)
 
-        with open('data/participants.pkl', 'wb') as f:
-            pickle.dump(list_participants, f)
+    username = update.message.from_user.username
+    if username not in df_subscribers['username'].values:
+        new_nfc_id = uuid.uuid4()
+        new_sub = pd.DataFrame({'username': [username], 'nfc_id': [new_nfc_id]})
+        df_subscribers = df_subscribers.append(new_sub, ignore_index=True)
 
         context.bot.send_message(chat_id=update.message.chat_id,
-                                 text="Participants added")
+                                 text=f"You have subscribed to our service. Your NFC tag id is:\n{new_nfc_id}\nPlease keep it.\nTo use our machines, download a nfc tag maker on your phone, and create a NFC tag with your given id")
+
     else:
         context.bot.send_message(chat_id=update.message.chat_id,
-                                 text="Couple assignement has already been done, it's not possible to add more participants now.")
+                                 text=f"You are already subscribed to our service\nPlease use contact an administrator of you forgot your NFC ID, or use /unsubscribe to remove your subscription")
+
+    with open('data/subscribers.pkl', 'wb') as f:
+        pickle.dump(df_subscribers, f)
 
 
-def assign(update, context):
+def unsubscribe(update, context):
     """
-    PROTECTED
-    Uses the list of participants to generate the pairings, and store these pairings
-    """
-
-    try:
-        with open('data/participants.pkl', 'rb') as f:
-            list_participants = pickle.load(f)
-
-        print('loaded')
-
-        dict_couples = chain(list_participants)
-
-        print('chained')
-
-        with open('data/assignements.pkl', 'wb') as f:
-            pickle.dump(dict_couples, f)
-
-        context.bot.send_message(chat_id=update.message.chat_id,
-                                 text="Assignements done")
-    except:
-        context.bot.send_message(chat_id=update.message.chat_id,
-                                 text="Participants list empty of invalid")
-
-
-def list_participants(update, context):
-    """
-    Returns the list of participants in the chat
+    Removes the username of the sender of the request form the list of subscribers
     """
     try:
-        with open('data/participants.pkl', 'rb') as f:
-            list_participants = pickle.load(f)
-
-        if len(list_participants) == 0:
-            context.bot.send_message(chat_id=update.message.chat_id,
-                                     text="There are no participants yet")
-        else:
-            text = ''
-            for username in list_participants:
-                text += f'{username}\n'
-            context.bot.send_message(chat_id=update.message.chat_id,
-                                     text=text)
+        with open('data/subscribers.pkl', 'rb') as f:
+            df_subscribers = pickle.load(f)
     except:
+        df_subscribers = pd.DataFrame(columns=['username', 'nfc_id'])
+
+    print(df_subscribers)
+
+    username = update.message.from_user.username
+    if username in df_subscribers['username'].values:
+        df_subscribers.drop(df_subscribers[df_subscribers['username'] == username].index, inplace=True)
         context.bot.send_message(chat_id=update.message.chat_id,
-                                 text="There are no participants yet")
+                                 text=f"You have successefully unsubscribed.")
 
-
-def join(update, context):
-    """
-    Adds the username that called /join to the list of participants
-    """
-    if not path.exists("data/assignements.pkl"):
-        try:
-            with open('data/participants.pkl', 'rb') as f:
-                list_participants = pickle.load(f)
-        except:
-            list_participants = []
-
-        username = '@' + update.message.from_user.username
-        if username not in list_participants:
-            list_participants.append(username)
-
-        with open('data/participants.pkl', 'wb') as f:
-            pickle.dump(list_participants, f)
-
-        context.bot.send_message(chat_id=update.message.chat_id,
-                                 text="You have been added")
     else:
         context.bot.send_message(chat_id=update.message.chat_id,
-                                 text="Couple assignement has already been done, it is too late to join now")
+                                 text=f"You aren't subscribed to our service, please consider using /subscribe to do so.")
+
+    with open('data/subscribers.pkl', 'wb') as f:
+        pickle.dump(df_subscribers, f)
 
 
-def get_name(update, context):
-    """
-    Returns the name chosen for the username that called /get_name
-    """
-    try:
-        with open('data/assignements.pkl', 'rb') as f:
-            dict_couples = pickle.load(f)
-
-        username = update.message.from_user.username
-        text = f'You will be giving a gift to {dict_couples["@" + username]}'
-        context.bot.send_message(chat_id=update.message.chat_id,
-                                 text=text)
-    except:
-        context.bot.send_message(chat_id=update.message.chat_id,
-                                 text="Couple assignement hasn't been done yet")
-
-
-def get_all(update, context):
-    """
-    PROTECTED, TEST ONLY
-    Returns the name chosen for the username that called /get_name
-    """
-    try:
-        with open('data/assignements.pkl', 'rb') as f:
-            dict_couples = pickle.load(f)
-
-        text = ''
-        for giver in dict_couples:
-            text += f'{giver} gives a gift to {dict_couples[giver]}\n'
-        context.bot.send_message(chat_id=update.message.chat_id,
-                                 text=text)
-    except:
-        context.bot.send_message(chat_id=update.message.chat_id,
-                                 text="Couple assignement hasn't been done yet")
+def status(update, context):
+    context.bot.send_message(chat_id=update.message.chat_id,
+                             text=f"PLACEHOLDER message for status updates")
--- src/main.py
+++ src/main.py
@@ -1,12 +1,19 @@
 from functions import *
 import logging
+import time
+
 from telegram.ext import Updater, Filters, CommandHandler
 
-with open('data/token.txt', 'r') as f:
+with open('data/token_bot', 'r') as f:
     token = f.read().strip()
     updater = Updater(token=token, use_context=True)
 
 dispatcher = updater.dispatcher
+
+with open('data/token_card', 'r') as f:
+    tokens = f.read().strip().split('\n')
+    token_write = tokens[0]
+    token_read = tokens[1]
 
 # ------------------------------------------------------------------------------
 
@@ -18,33 +25,24 @@ logging.basicConfig(level=logging.DEBUG,
 
 # Get admins list
 admins = []
-with open('data/admins.txt', 'r') as f:
+with open('data/admins', 'r') as f:
     admins = f.read().strip()
 
 # ------------------------------------------------------------------------------
 
-start_handler = CommandHandler('start', start)
+start_handler = CommandHandler('start', start, filters=Filters.user(username=admins))
 dispatcher.add_handler(start_handler)
 
 reset_handler = CommandHandler('reset', reset, filters=Filters.user(username=admins))
 dispatcher.add_handler(reset_handler)
 
-add_handler = CommandHandler('add', add, filters=Filters.user(username=admins))
-dispatcher.add_handler(add_handler)
+subscribe_handler = CommandHandler('subscribe', subscribe)
+dispatcher.add_handler(subscribe_handler)
 
-assign_handler = CommandHandler('assign', assign, filters=Filters.user(username=admins))
-dispatcher.add_handler(assign_handler)
+unsubscribe_handler = CommandHandler('unsubscribe', unsubscribe)
+dispatcher.add_handler(unsubscribe_handler)
 
-list_participants_handler = CommandHandler('list', list_participants)
-dispatcher.add_handler(list_participants_handler)
-
-join_handler = CommandHandler('join', join)
-dispatcher.add_handler(join_handler)
-
-get_name_handler = CommandHandler('get_name', get_name)
-dispatcher.add_handler(get_name_handler)
-
-get_all_handler = CommandHandler('get_all', get_all, filters=Filters.user(username=admins))
-dispatcher.add_handler(get_all_handler)
+status_handler = CommandHandler('status', status)
+dispatcher.add_handler(status_handler)
 
 updater.start_polling()
